45181c0cdd3de489ec2193ac88986141e74aad14
Marko126bit <128129906+Marko126bit@users.noreply.github.com>
2024-01-07 18:42:12 +0100

    Add files via upload

---
+++ Project/app/forms.py
@@ -0,0 +1,30 @@
+# app/forms.py
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, RadioField, StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Length
+ 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class StorageForm(FlaskForm):
+    name = StringField('Storage Name', validators=[DataRequired()])
+    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    action = RadioField('Action', choices=[('give', 'Give'), ('take', 'Take')], default='give')
+    submit = SubmitField('Update Storage')
+
+    def validate_quantity(form, field):
+        if field.data < 0:
+            raise ValidationError('Quantity must be a non-negative integer.')
+        
+class SmallStorageForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    submit = SubmitField('Create Small Storage')
+
+
+class PartnerForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    submit = SubmitField('Create Partner')
