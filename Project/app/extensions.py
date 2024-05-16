ec7d6dc363526e5a69b4151f379e4f51d6fc653f
Marko126bit <128129906+Marko126bit@users.noreply.github.com>
2024-05-16 23:42:05 +0200

    Add files via upload

---
+++ Project/app/extensions.py
@@ -0,0 +1,13 @@
+# extensions.py
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_caching import Cache
+
+
+cache = Cache()
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+
